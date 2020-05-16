1562eb0502ff4c0f7bfb2d8c559f62817e76eaec
Inaayat <59137672+Inaayat04@users.noreply.github.com>
2020-05-16 10:11:19 +0400

    Create Main.php

---
+++ src/Inaayat/NoDrop/Main.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Inaayat\NoDrop;
+
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDropItemEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\utils\TextFormat as TF;
+
+class Main extends PluginBase implements Listener {
+
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onDrop(PlayerDropItemEvent $event)
+    {
+        $player = $event->getPlayer();
+        if ($player instanceof Player) {
+            if ($player->isOp()) {
+                return;
+            } else {
+                $event->setCancelled(true);
+                $player->sendMessage("[" . TF::AQUA . "NoDrop" . TF::RESET . "] " . TF::YELLOW . "You cannot drop here.");
+            }
+        }
+    }
+
+    public function onDeath(PlayerDeathEvent $event){
+        $player = $event->getPlayer();
+        if($player instanceof Player){
+            $event->setDrops([" "]);
+        }
+    }
+}
