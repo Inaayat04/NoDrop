95b76ba5d2b808f8471f73228a49f4c21c7955bc
Inaayat <59137672+Inaayat04@users.noreply.github.com>
2020-05-26 08:27:32 +0400

    Update EventListener.php

--- src/Inaayat/NoDrop/EventListener.php
+++ src/Inaayat/NoDrop/EventListener.php
@@ -1,1 +1,33 @@
+<?php
 
+namespace Inaayat\NoDrop;
+
+use pocketmine\Player;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDropItemEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\utils\TextFormat as TF;
+use Inaayat\NoDrop\Main;
+
+class EventListener implements Listener {
+  
+  private $plugin;
+  
+  public function __construct(Main $plugin){
+        $this->plugin = $plugin;
+  }
+
+    public function onDrop(PlayerDropItemEvent $event)
+    {
+        $player = $event->getPlayer();
+        if ($player instanceof Player) {
+            if ($player->hasPermission("bypass.nodrop")) {
+                return;
+            } else {
+                $event->setCancelled(true);
+                $nodropmsg = $this->plugin->config->get("NoDropMsg);
+                $player->sendMessage("[" . TF::AQUA . "NoDrop" . TF::RESET . "] " . TF::YELLOW . $nodropmsg);
+            }
+        }
+   } 
+}
