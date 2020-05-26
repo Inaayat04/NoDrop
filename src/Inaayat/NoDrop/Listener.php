573fb16ebf658b761495a680cf2ae74d3df80c35
Inaayat <59137672+Inaayat04@users.noreply.github.com>
2020-05-26 08:28:40 +0400

    Create Listener.php

---
+++ src/Inaayat/NoDrop/Listener.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Inaayat\NoDrop;
+
+use pocketmine\Player;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDeathEvent;
+use Inaayat\NoDrop\Main;
+
+class Listener implements Listener {
+  
+  private $plugin;
+  
+  public function __construct(Main $plugin){
+        $this->plugin = $plugin;
+  }
+
+    public function onDeath(PlayerDeathEvent $event){
+        $player = $event->getPlayer();
+        if($player instanceof Player){
+            $event->setDrops([ ]);
+        }
+    }
+}
