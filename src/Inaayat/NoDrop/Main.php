8a8041c9dfa31fcac2ace387b6824090c27d0db4
Inaayat <59137672+Inaayat04@users.noreply.github.com>
2020-05-26 08:33:06 +0400

    Update Main.php

--- src/Inaayat/NoDrop/Main.php
+++ src/Inaayat/NoDrop/Main.php
@@ -2,36 +2,22 @@
 
 namespace Inaayat\NoDrop;
 
-use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerDropItemEvent;
-use pocketmine\event\player\PlayerDeathEvent;
-use pocketmine\utils\TextFormat as TF;
+use pocketmine\utils\Config;
+use Inaayat\NoDrop\EventListener;
+use Inaayat\NoDrop\Listener;
 
-class Main extends PluginBase implements Listener {
+class Main extends PluginBase {
 
     public function onEnable(){
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    }
-
-    public function onDrop(PlayerDropItemEvent $event)
-    {
-        $player = $event->getPlayer();
-        if ($player instanceof Player) {
-            if ($player->hasPermission("bypass.nodrop")) {
-                return;
-            } else {
-                $event->setCancelled(true);
-                $player->sendMessage("[" . TF::AQUA . "NoDrop" . TF::RESET . "] " . TF::YELLOW . " You cannot drop here.");
-            }
+        @mkdir($this->getDataFolder());
+		$this->saveDefaultConfig();
+		$this->config = $this->getConfig();
+        if($this->config->get("NoDropWhenDie" === true)){
+            $this->getServer()->getPluginManager()->registerEvents(new Listener($this), $this);
         }
-    }
-
-    public function onDeath(PlayerDeathEvent $event){
-        $player = $event->getPlayer();
-        if($player instanceof Player){
-            $event->setDrops([ ]);
+        if($this->config->get("NoItemDrop" === true)){
+            $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
         }
     }
 }
