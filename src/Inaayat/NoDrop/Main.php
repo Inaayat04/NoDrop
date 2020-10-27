ffe74fb5c98e0f4040f7c87d256d942c94fb0a37
KygekDev <68368066+Kygekraqmak@users.noreply.github.com>
2020-10-27 15:41:15 +0800

    Removed `mkdir()` because it is unnecessary

--- src/Inaayat/NoDrop/Main.php
+++ src/Inaayat/NoDrop/Main.php
@@ -4,20 +4,20 @@ namespace Inaayat\NoDrop;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
-use Inaayat\NoDrop\EventListener;
-use Inaayat\NoDrop\Listeners;
+use Inaayat\NoDrop\event\PlayerDropItemsEvent;
+use Inaayat\NoDrop\event\PlayerDeathItemDropsEvent;
 
 class Main extends PluginBase {
 
     public function onEnable(){
-        @mkdir($this->getDataFolder());
 	$this->saveDefaultConfig();
 	$this->config = $this->getConfig();
         if($this->config->get("NoDropWhenDie" === true)){
-            $this->getServer()->getPluginManager()->registerEvents(new Listeners($this), $this);
+            $this->getServer()->getPluginManager()->registerEvents(new PlayerDropItemsEvent($this), $this);
         }
         if($this->config->get("NoItemDrop" === true)){
-            $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+            $this->getServer()->getPluginManager()->registerEvents(new PlayerDeathItemDropsEvent($this), $this);
         }
     }
+
 }
